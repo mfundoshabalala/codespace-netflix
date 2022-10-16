063c826ffd1467f171848d71fd8c30d86a7444b4
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-10-16 23:42:06 +0200

    Update image location

--- src/components/StoryCard.vue
+++ src/components/StoryCard.vue
@@ -14,14 +14,14 @@ export default {
       <h2>{{ subheading }}</h2>
     </div>
     <div>
-      <img src="../assets/placeholder.jpg" alt="placeholder" />
+      <img src="../assets/images/placeholder.jpg" alt="placeholder" />
     </div>
   </section>
 </template>
 
 <style>
 section {
-  @apply flex justify-center border-b-8 border-slate-600 w-full px-2;
+  @apply flex justify-center border-b-8 border-slate-600 w-full container;
 }
 
 section:nth-of-type(2n) {
@@ -29,7 +29,7 @@ section:nth-of-type(2n) {
 }
 
 section > div {
-  @apply flex-1;
+  @apply flex-1 max-w-2xl px-8;
 }
 
 section > div:last-child {
@@ -37,7 +37,7 @@ section > div:last-child {
 }
 
 section img {
-  @apply h-full w-full;
+  @apply h-full w-full mix-blend-lighten;
 }
 
 section > div:first-child {
