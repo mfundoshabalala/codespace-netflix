18e4fd4d13ad9e26578d4c919ddd85927d58aaa4
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-10-16 23:44:11 +0200

    Instatiate the data store

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import store from "./store";
+//
 import "./assets/main.css";
+import "./assets/tailwind.css";
 
 const app = createApp(App);
+
+app.use(store);
 
 app.use(router);
 
