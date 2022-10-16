8ac3eb8dd4879be47a2d3ef87e73b68f8b4a0af4
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-10-16 16:57:14 +0200

    rename navigation component

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <!-- site branding -->
+    <router-link to="/landing">
+      <img src="../assets/images/netflix-logo.svg" alt="Netflix" />
+    </router-link>
+    <!-- site auth link -->
+    <router-link to="/login" class="button">Sign in</router-link>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped>
+nav {
+  // nav wrapper styling
+  @apply shadow-md bg-transparent z-50;
+  @apply flex flex-row justify-between items-center;
+  // brand styling
+  a img {
+    @apply sm:w-52 max-h-16;
+  }
+  // auth button styling
+  a.button {
+    @apply bg-red-600 px-4 py-1.5 rounded-sm mx-4;
+  }
+}
+</style>
