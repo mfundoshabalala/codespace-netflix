445d925b9206924a2204b085a192314e103385cc
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-11-02 15:40:53 +0200

    main.js commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 //
 import "./assets/main.css";
 import "./assets/tailwind.css";
 
 const app = createApp(App);
 
-app.use(store);
+app.use(createPinia());
 
 app.use(router);
 
